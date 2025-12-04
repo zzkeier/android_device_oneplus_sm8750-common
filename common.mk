8cbc5957ce7ac84fc10c980acafa05a97a4a1472
z <z@ze.fyi>
2025-12-04 22:24:12 +0800

    Revert "sm8750-common: Drop UDFPS extension"
    This reverts commit 454cf98b4a5c5f59b8c472c33c4de83467a6c16a.

--- common.mk
+++ common.mk
@@ -174,6 +174,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 endif
 
+$(call soong_config_set,surfaceflinger,udfps_lib,//hardware/oplus:libudfps_extension.oplus)
+
 # GPS
 ifneq ($(TARGET_IS_TABLET),true)
 PRODUCT_COPY_FILES += \
