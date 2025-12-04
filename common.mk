c2b6632252263ff13b8565973f7be52e43175c6c
z <z@ze.fyi>
2025-12-04 22:23:11 +0800

    pagani unsupport

--- common.mk
+++ common.mk
@@ -27,11 +27,11 @@ PRODUCT_PACKAGES += \
     otapreopt_script
 
 # Alert slider
-ifneq ($(TARGET_IS_TABLET),true)
-PRODUCT_PACKAGES += \
-    KeyHandler \
-    tri-state-key-calibrate
-endif
+# ifneq ($(TARGET_IS_TABLET),true)
+# PRODUCT_PACKAGES += \
+#     KeyHandler \
+#     tri-state-key-calibrate
+# endif
 
 # Audio
 PRODUCT_PACKAGES += \
